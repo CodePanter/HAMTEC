6be300638714e5dacb412e91a3fafdcefc1523f1
Moarten <sailor1991@msn.com>
2013-03-14 20:00:54 +0100

    My account pagina toegevoegd
    Signed-off-by: Moarten <sailor1991@msn.com>

---
+++ Hamtec/Hamtec/Account/myaccount.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+namespace Hamtec.Account
+{
+    public partial class myaccount : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] != null)
+            {
+                
+
+            }
+            else
+            {
+                //Response.Redirect("/Default.aspx");
+            }
+        }
+    }
+}
