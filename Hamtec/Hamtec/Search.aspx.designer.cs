12cecebbd52fc053fead017b054fea2c4d876701
commandersatisfied <huppel31@hotmail.com>
2013-04-17 16:18:17 +0200

    Fixed Tags, added features to search.
    Signed-off-by: commandersatisfied <huppel31@hotmail.com>

--- Hamtec/Hamtec/Search.aspx.designer.cs
+++ Hamtec/Hamtec/Search.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Hamtec {
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
@@ -29,6 +38,15 @@ namespace Hamtec {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
         
         /// <summary>
         /// SqlDataSource1 control.
